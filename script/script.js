3c241734cfc391f051ce5c4f4568803134d1d38a
MrMishka02 <m.gogia@hotmail.com>
2022-11-15 00:09:01 +0400

    docs: added json data

--- script/script.js
+++ script/script.js
@@ -1,10 +1,13 @@
 fetch('../books.json')
-        .then(response => {
-            return response.json();
-        })
-        .then(data => {
-            console.log(data);
-        });
+.then(function (response) {
+    return response.json();
+})
+.then(function (data) {
+    appendData(data);
+})
+.catch(function (err) {
+    console.log('error: ' + err);
+});
 
 //fisrt section
 const div1 = document.createElement('div');
@@ -19,14 +22,27 @@ span2.className = 'spans';
 span1.innerText = "welcome";
 span2.innerText = "discover the best books with us";
 
-//second section
+//Collection
 const div2 = document.createElement('div');
 document.body.append(div2);
-div2.className = 'sect2';
-const myH1 = document.createElement('h1')
+div2.className = "div2"
+const myH1 = document.createElement('h1');
+myH1.className = "myh1";
+myH1.innerHTML = "Books Catalog";
 div2.appendChild(myH1);
-myH1.innerText = "Books Catalog";
-myH1.className = 'myh1'
+
+//Second section
 const div3 = document.createElement('div');
-div2.appendChild(div3);
-div3.className = 'container';
+document.body.append(div3);
+div3.id = 'sect2';
+
+//Books
+function appendData(data) {
+    const books = document.getElementById("sect2");
+    for (var i = 0; i < data.length; i++) {
+      var div4 = document.createElement("div");
+      div4.className = 'container';
+      div4.innerHTML = 'Author: ' + data[i].author + ' ' + data[i].title;
+      books.appendChild(div4)
+    }
+  }

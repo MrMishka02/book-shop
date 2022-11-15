fc6e114de28672a3233780770f7a42c109e726b0
MrMishka02 <m.gogia@hotmail.com>
2022-11-15 12:57:26 +0400

    docs: added json data

--- script/script.js
+++ script/script.js
@@ -36,13 +36,46 @@ const div3 = document.createElement('div');
 document.body.append(div3);
 div3.id = 'sect2';
 
-//Books
+//Books Catalog
 function appendData(data) {
     const books = document.getElementById("sect2");
     for (var i = 0; i < data.length; i++) {
       var div4 = document.createElement("div");
       div4.className = 'container';
-      div4.innerHTML = 'Author: ' + data[i].author + ' ' + data[i].title;
-      books.appendChild(div4)
+      var img = document.createElement("img");
+      img.className = "book_img";
+      var auth_p = document.createElement("p");
+      auth_p.className = "auth";
+      var title_p = document.createElement("p");
+      title_p.className = "title";
+      var price_p = document.createElement("p");
+      price_p.className = "price";
+      var btnDiv = document.createElement("div");
+      btnDiv.className = "btnDiv";
+      var btnPop = document.createElement("button");
+      btnPop.className = "btnPop";
+      btnPop.innerHTML = "Show more";
+      var btnBag = document.createElement("button");
+      btnBag.className = "btnBag";
+      btnBag.innerHTML = "Add to Bag";
+      btnDiv.appendChild(btnPop);
+      btnDiv.appendChild(btnBag);
+      img.src = data[i].imageLink;
+      auth_p.innerHTML = data[i].author;
+      title_p.innerHTML = data[i].title;
+      price_p.innerHTML = data[i].price + "$";
+      div4.appendChild(img);
+      div4.appendChild(auth_p);
+      div4.appendChild(title_p);
+      div4.appendChild(price_p);
+      div4.appendChild(btnDiv);
+      books.appendChild(div4);
     }
   }
+function popup() {
+    let pop_p = document.createElement("p");
+    pop_p.className = "popuptext";
+    pop_p.id = "myPopup";
+    pop_p.innerHTML = data[i].description;
+    
+}
